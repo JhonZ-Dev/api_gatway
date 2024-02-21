682b5401b4929bf95205362bc4943850672e28f7
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-21 08:59:07 -0500

    Update SpringApiGatewayApplication.java

--- spring_api_gateway/src/main/java/com/example/spring_api_gateway/SpringApiGatewayApplication.java
+++ spring_api_gateway/src/main/java/com/example/spring_api_gateway/SpringApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.example.spring_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringApiGatewayApplication {
 
 	public static void main(String[] args) {
